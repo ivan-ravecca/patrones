ade955733a9ff251f385c5f1d58983ed1df708f3
Ivan Sanchez <ivan.sanchez@autodesk.com>
2022-08-04 12:21:54 -0300

    adding memoize

--- src/services/fakeServerCall.js
+++ src/services/fakeServerCall.js
@@ -1,18 +1,25 @@
 import isThisTheRealWorld from '../globals';
+import helpers from '../utilities/helpers';
+const internalGet = (queryString) => {
+	if(isThisTheRealWorld) console.log(`	FakeServerCall.get(${queryString})`);
+	return new Promise((resolve, reject) => {
+		const magicNumberPromise = Math.floor(Math.random() * 2000);
+		setTimeout(() => {
+			const result = {
+				queryString: queryString,
+				time: new Date()
+			};
+			if(isThisTheRealWorld) console.log(`	FakeServerCall.get:Result: ${JSON.stringify(result)} it took ${magicNumberPromise} ms`);
+			resolve(result);
+		}, magicNumberPromise);
+	});
+};
+const memoizedGet = helpers.memoize();
+
 const FakeServerCall = {
-	get: (queryString) => {
-		if(isThisTheRealWorld) console.log(`	FakeServerCall.get(${queryString})`);
-		return new Promise((resolve, reject) => {
-			const magicNumberPromise = Math.floor(Math.random() * 2000);
-			setTimeout(() => {
-				const result = {
-					queryString: queryString,
-					time: new Date()
-				};
-				if(isThisTheRealWorld) console.log(`	FakeServerCall.get:Result: ${JSON.stringify(result)} it took ${magicNumberPromise} ms`);
-				resolve(result);
-			}, magicNumberPromise);
-		});
+	// get: internalGet
+	get: (queryString) => {		
+		return memoizedGet(queryString, internalGet);
 	}
 };
 
--- src/utilities/helpers.js
+++ src/utilities/helpers.js
@@ -28,4 +28,32 @@ const customThrottle = (funcCall, interval=500) => {
 	}
 };
 
-export default {customDebounce, customThrottle};
+const memoize = () => {
+	let cache = [];
+	return (key, funCall) => {
+		if (cache[key]) {
+			if(isThisTheRealWorld) console.info(`memoize - return from cache key: ${key}`);
+			return cache[key];
+		}
+		if(isThisTheRealWorld) console.info(`memoize - STORING cache key: ${key}`);
+		cache[key] = funCall(key);
+		return cache[key];
+	};
+};
+
+const queue = () => {
+	let internalQueue = [];
+	return (key, funCall) => {
+		if (internalQueue[key]) {
+			if(isThisTheRealWorld) console.info('memoize - return from cache');
+			internalQueue[key].cancel();
+		}
+		internalQueue[key] = {
+			cancel: ()=>{},
+			result: funCall()
+		};
+		return internalQueue[key].result;
+	};
+};
+
+export default {customDebounce, customThrottle, memoize, queue};
